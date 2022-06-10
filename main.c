1bcac94c2a7e03dbc8b860862ff27ca5d38994ce
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-11 00:52:10 +0100

    changing variable

--- main.c
+++ main.c
@@ -4,7 +4,17 @@
 *main - Naming identifiers.
 *Return: return (0).
 */
+
 int main() {
+
+    int Age = 23;
+
+
+    printf("Hello Twitter, This is my first C programme ");
+
+    Age = 24;
+
+    printf("This is my new age: %d", Age);
 
 
     return 0;
