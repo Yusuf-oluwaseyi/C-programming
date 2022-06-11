a1ea82fb0c30045b71d1348b850f3638ed2e88aa
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-11 01:11:59 +0100

    Variables and constant

--- main.c
+++ main.c
@@ -10,11 +10,15 @@ int main() {
     int Age = 22;
 
 
-    printf("Hello Twitter, This is my first C programme ");
+    printf("Hello Twitter, This is my first C programme\n");
 
     Age = 23;
 
     printf("This is my new age: %d", Age);
+
+    int Car1, car2 = 25;
+
+    printf("\n My car is %d %d", car2, Car1);
 
 
     return 0;
