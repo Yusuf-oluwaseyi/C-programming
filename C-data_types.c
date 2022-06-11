5d47b2cc5dc7445b8f84d76483e1cfcdec633b58
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-11 02:54:35 +0100

    size of other operators

--- C-data_types.c
+++ C-data_types.c
@@ -19,5 +19,19 @@ int main() {
 
     printf("\nPrint size of double = %zu", sizeof (number));
 
+    short a;
+    long b;
+    long long c;
+    long double d;
+    unsigned int e;
+    signed char f;
+
+    printf("\nPrint size of short = %hd", sizeof (a));
+    printf("\nPrint size of long = %hd", sizeof (b));
+    printf("\nPrint size of long long = %hd", sizeof (c));
+    printf("\nPrint size of unsigned int = %hd", sizeof (e));
+    printf("\nPrint size of long double = %hd", sizeof (d));
+    printf("\nPrint size of signed char = %hd", sizeof (f));
+
     return 0;
 }
