6ac17394509dee3c8c06ddc2a9d3129f3ed7600d
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-12 14:49:53 +0100

    commenting in c

---
+++ C-comment.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * @brief - long comment is /* .
+ * short comment is //
+ * 
+ * 
+ */
+int main() {
+
+    /*int age;
+
+    printf("What is your age: \n");
+    scanf("%d", &age);
+
+    printf("My age is %d", age);*/
+
+    char car[] = "Ferrari";
+    //int size = 7;//
+
+    printf("I own a %s", car);
+
+    return 0;
+}
