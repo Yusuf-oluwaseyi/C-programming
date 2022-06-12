aee23fc62f65717e9f3fc73ff4d182ddcb46395c
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-12 23:34:14 +0100

    while loop

---
+++ while.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * @brief - while and do while loop.
+ * The difference explained.
+ * Return: no return.
+ * 
+ */
+int main() {
+    // while (condition) {
+        // statement inside while
+    // }
+    // infinite loop
+    /* while (1 < 5) {
+        printf("While loop is c\n")
+    }
+    return 0;
+    */
+
+//    finite loop
+    int count = 1;
+
+    while (count < 5) {
+        printf("While loop is c\n");
+        count = count + 1;
+    }
+
+
+    return 0;
+}
