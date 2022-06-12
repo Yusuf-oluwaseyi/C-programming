4a4fb63b9ec849082cf1f2d91016a129e09691b9
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-12 12:32:43 +0100

    using scanf

---
+++ Output.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * @brief - input/output
+ * displaying output scanf & printf.
+ * Return: return.
+ * 
+ */
+int main() {
+
+    int age = 
+
+    printf("Please enter your desired age: \n");
+
+    scanf("%d", &age);
+
+    printf("You are %d year old", age);
+
+
+    return 0;
+}
