d52273aa1560c5ffd2a0637828a86a23f6e4fd22
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-12 16:11:34 +0100

    if else if

--- flowcontrol.c
+++ flowcontrol.c
@@ -7,7 +7,7 @@ int main(){
 
     //if just printing one c or working with one
     //no need for {}
-    int age;
+    /*int age;
 
     printf("Enter your age: \n");
     scanf("%d", &age);
@@ -17,6 +17,27 @@ int main(){
     }
     else {
         printf("Sorry my baby.");
+    }
+*/
+    //if, else if, else statement.
+
+    int age;
+
+    printf("Please enter your age: \n");
+    scanf("%d", &age);
+
+    if (age > 150){
+        //if (age > 150 || age < 0)
+        printf("Invalid age to vote");
+    }
+    else if (age < 0){
+        printf("Invalid age to vote");
+    }
+    else if (age >= 18) {
+        printf("You are eligible to vote");
+    }
+    else {
+        printf("Sorry baby");
     }
 
     return 0;
