b6f471634db96e526dbf855690704cddd22c67f8
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-19 15:34:48 +0100

    Adding functions in c

---
+++ Fuction.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * @brief - Function in c.
+ * Use descriptive name for function.
+ * 
+ * 
+ */
+// syntax
+/* returnType functionName () {
+
+
+}
+*/
+
+//    using void when it brings no return.
+   void greet() {
+    printf("Good Evening Techies!");
+   }
+
+   int main() {
+
+    greet();
+
+    return 0;
+   }
