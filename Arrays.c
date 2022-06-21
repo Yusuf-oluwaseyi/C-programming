26da65a2bee5db36a0c88c8d80ad9db0abb8d0bc
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-21 23:42:31 +0100

    Arrays in c

---
+++ Arrays.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * @brief - Arrays in c
+ * 
+ */
+
+// datatype arrayName [arraySize]; syntax
+int age[5];
+int age[5] = {12, 67, 48, 1, 25};
---
+++ P&A.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+/**
+ * @brief - Pointers and Arrays.
+ * 
+ */
