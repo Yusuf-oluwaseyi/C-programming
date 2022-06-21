457b3943e618b6590b717da244dadacf57b5274f
Yusuf-oluwaseyi <85449521+Yusuf-oluwaseyi@users.noreply.github.com>
2022-06-21 22:03:36 +0100

    More pointers

--- pointer.c
+++ pointer.c
@@ -52,3 +52,45 @@ Note: In the above example, pc is a pointer, not *pc. You cannot and should not 
 
 By the way, * is called the dereference operator (when working with pointers). 
 It operates on a pointer and gives the value stored in that pointer.
+
+
+Changing Value Pointed by Pointers
+
+Let's take an example.
+
+int* pc, c;
+c = 5;
+pc = &c;
+c = 1;
+printf("%d", c);    // Output: 1
+printf("%d", *pc);  // Ouptut: 1
+
+We have assigned the address of c to the pc pointer.
+
+Then, we changed the value of c to 1. Since pc and the address of c is the same, *pc gives us 1.
+
+Let's take another example.
+
+int* pc, c;
+c = 5;
+pc = &c;
+*pc = 1;
+printf("%d", *pc);  // Ouptut: 1
+printf("%d", c);    // Output: 1
+
+We have assigned the address of c to the pc pointer.
+
+Then, we changed *pc to 1 using *pc = 1;. Since pc and the address of c is the same, c will be equal to 1.
+
+Let's take one more example.
+
+int* pc, c, d;
+c = 5;
+d = -15;
+
+pc = &c; printf("%d", *pc); // Output: 5
+pc = &d; printf("%d", *pc); // Ouptut: -15
+
+Initially, the address of c is assigned to the pc pointer using pc = &c;. Since c is 5, *pc gives us 5.
+
+Then, the address of d is assigned to the pc pointer using pc = &d;. Since d is -15, *pc gives us -15.
